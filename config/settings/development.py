63cf374cbda0428fd1618d064482a394647c0698
Kamoliddin Fazliddinov <komoliddin487@gmail.com>
2026-03-03 11:51:04 +0500

    chore: Update dependencies to include mypy and python-discovery, and modify settings structure

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,15 +2,15 @@
 
 from typing import Optional
 
-from .base import BASE_DIR  # aniq import
+from .base import *  # noqa: F403,F405
 
-DEBUG: bool = True
-ALLOWED_HOSTS: list[str] = ["localhost", "127.0.0.1"]
+DEBUG: bool = True  # type: ignore[no-redef]
+ALLOWED_HOSTS: list[str] = ["localhost", "127.0.0.1"]  # type: ignore[no-redef]
 CORS_ALLOW_ALL_ORIGINS: bool = True
 
 DATABASES: dict[str, dict[str, Optional[str]]] = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(BASE_DIR / "db.sqlite3"),  # <-- Path -> str
+        "NAME": str(BASE_DIR / "db.sqlite3"),  # noqa: F405
     }
 }
