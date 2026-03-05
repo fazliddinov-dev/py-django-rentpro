750506e66f75854e995c3096a79505548f478171
Kamoliddin Fazliddinov <komoliddin487@gmail.com>
2026-03-05 15:46:46 +0500

    Add super admin auth partially

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
-    "core.user",
 ]
 
 # Middleware
--- config/urls.py
+++ config/urls.py
@@ -18,4 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("api.v1.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
