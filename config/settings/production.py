c9aa009e728b03546cc011b1e57f1266c4d52737
komoliddin <komoliddin487@gmail.com>
2026-03-25 12:08:00 +0500

    add postgres configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,13 +8,4 @@ import dj_database_url
 DEBUG: bool = False
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"), conn_max_age=600
-    )
-}
-
-# Qo‘shimcha security settings
-CSRF_COOKIE_SECURE: bool = True
-SESSION_COOKIE_SECURE: bool = True
-SECURE_SSL_REDIRECT: bool = True
+DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
