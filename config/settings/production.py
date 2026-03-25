97f39d10a582cd25cc272c7f5ab7711f2dd697cd
komoliddin <komoliddin487@gmail.com>
2026-03-25 12:10:09 +0500

    add postgres configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,5 +7,7 @@ import dj_database_url
 
 DEBUG: bool = False
 ALLOWED_HOSTS = ["*"]
+ROOT_URLCONF: str = "config.urls"
+
 
 DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
